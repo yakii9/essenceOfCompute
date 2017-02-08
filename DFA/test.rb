c5e6b08282ba91c796224dfc2b49067d339c0bf3
段诗闻 <yakii9@vip.qq.com>
2017-02-08 14:23:12 +0800

    添加测试自动机脚本

---
+++ DFA/test.rb
@@ -0,0 +1,15 @@
+require './machine.rb'
+require './rule.rb'
+
+rules = [
+  FARule.new(1, 'a', 2),
+  FARule.new(2, 'b', 3),
+  FARule.new(3, 'c', 1)
+]
+
+book = RuleBook.new(rules)
+
+machine = Machine.new(1, [3], book)
+puts machine.accepting?
+machine.read_string('ab')
+puts machine.accepting?
